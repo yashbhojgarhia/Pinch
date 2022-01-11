c944931bd7cbf3c9379c3edcd5ef44760cc2b7f0
Yash Bhojgarhia <yashbhojgarhia@gmail.com>
2022-01-11 14:47:18 +0530

    added control buttons

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Yash Bhojgarhia on 11/01/22.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
